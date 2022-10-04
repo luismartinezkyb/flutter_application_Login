d88c47e82828e8d61bdc10dd5197bdd0d951d5ce
Luis Martinez <18030545@itcelaya.edu.mx>
2022-10-03 21:07:39 -0500

    onboarding screen finished 100% with all the animations and with animatedText

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,6 +1,8 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -25,32 +27,144 @@ class _OnboardingPageState extends State<OnboardingPage> {
     super.dispose();
   }
 
+  final colorizeColors = [
+    Colors.purple,
+    Colors.blue,
+    Colors.yellow,
+    Colors.red,
+  ];
+
+  final colorizeTextStyle = TextStyle(
+    fontSize: 62.0,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Horizon',
+  );
   Widget buildPage({
     required Color color,
     required String urlImage,
     required String backgroundImage,
     required String title,
     required String subtitle,
+    required List<AnimatedText> frases,
   }) {
     return Container(
-        decoration: BoxDecoration(
-          color: color,
-          image: DecorationImage(
-            image: AssetImage(backgroundImage),
-            fit: BoxFit.cover,
-          ),
+      decoration: BoxDecoration(
+        color: color,
+        image: DecorationImage(
+          image: AssetImage(backgroundImage),
+          fit: BoxFit.cover,
         ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: MediaQuery.of(context).size.width / 1.5,
-              child: Image.asset(
-                urlImage,
-                width: MediaQuery.of(context).size.width / 1,
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            top: MediaQuery.of(context).size.width / 1.5,
+            child: Image.asset(
+              urlImage,
+              width: MediaQuery.of(context).size.width / 1,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.width / 7,
+            child: AnimatedTextKit(
+              animatedTexts: [
+                ColorizeAnimatedText(
+                  title,
+                  speed: Duration(milliseconds: 700),
+                  textStyle:
+                      GoogleFonts.almendraSc(textStyle: colorizeTextStyle),
+                  colors: colorizeColors,
+                ),
+                ColorizeAnimatedText(
+                  title,
+                  speed: Duration(milliseconds: 700),
+                  textStyle:
+                      GoogleFonts.almendraSc(textStyle: colorizeTextStyle),
+                  colors: colorizeColors,
+                ),
+                ColorizeAnimatedText(
+                  title,
+                  speed: Duration(milliseconds: 700),
+                  textStyle:
+                      GoogleFonts.almendraSc(textStyle: colorizeTextStyle),
+                  colors: colorizeColors,
+                ),
+              ],
+              isRepeatingAnimation: true,
+              onTap: () {},
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 4,
+            child: Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width / 1.2,
+              height: MediaQuery.of(context).size.height / 10,
+              padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width / 15,
+                top: MediaQuery.of(context).size.height / 70,
+                bottom: MediaQuery.of(context).size.height / 65,
+                right: MediaQuery.of(context).size.width / 15,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: Theme.of(context).backgroundColor,
+              ),
+              child: DefaultTextStyle(
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(context).dialogBackgroundColor,
+                  fontSize: 20.0,
+                  fontFamily: 'Agne',
+                ),
+                child: AnimatedTextKit(
+                  totalRepeatCount: 1,
+                  onFinished: () {
+                    print('El contador es $contador');
+                    if (contador != 4) {
+                      controller.animateToPage(contador + 1,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn);
+                      //controller.jumpToPage(contador + 1);
+                    }
+                  },
+                  pause: Duration(milliseconds: 5000),
+                  isRepeatingAnimation: true,
+                  repeatForever: false,
+                  animatedTexts: frases,
+                  onTap: () {},
+                ),
               ),
             ),
-          ],
-        ));
+          )
+          // Center(
+          //   child:
+          //   // child: DefaultTextStyle(
+          //   style: const TextStyle(
+          //     fontSize: 30.0,
+          //     fontFamily: 'Agne',
+          //   ),
+          //   child: AnimatedTextKit(
+          //     animatedTexts: [
+          //       TypewriterAnimatedText(title),
+          //       TypewriterAnimatedText(title),
+          //       TypewriterAnimatedText(
+          //           'Do not patch bugs out, rewrite them'),
+          //       TypewriterAnimatedText(
+          //           'Do not test bugs out, design them out'),
+          //     ],
+          //     onTap: () {
+          //       print("Tap Event");
+          //     },
+          //   ),
+          // ),
+          // child: AnimatedTextKit(
+          //   animatedTexts: [TyperAnimatedText(title)],
+          // ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -69,16 +183,16 @@ class _OnboardingPageState extends State<OnboardingPage> {
               contador = value;
               switch (contador) {
                 case 0:
-                  tema.sethemeData(temaDia());
-                  break;
-                case 1:
                   tema.sethemeData(temaNoche());
                   break;
+                case 1:
+                  tema.sethemeData(temaDia());
+                  break;
                 case 2:
-                  tema.sethemeData(temaCalido());
+                  tema.sethemeData(temaAmarillo());
                   break;
                 case 3:
-                  tema.sethemeData(temaPerso());
+                  tema.sethemeData(temaAzul());
 
                   break;
                 case 4:
@@ -95,35 +209,91 @@ class _OnboardingPageState extends State<OnboardingPage> {
                 urlImage: 'assets/profesorWillow2.png',
                 title: 'Bienvenido',
                 subtitle:
-                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo'),
+                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo',
+                frases: [
+                  TypewriterAnimatedText('Bienvenido Maestro Pokemon...'),
+                  TypewriterAnimatedText('Soy el profesor Willow...'),
+                  TypewriterAnimatedText(
+                      'Y estoy aqui para enseñarte todo sobre este mundo...'),
+                  TypewriterAnimatedText(
+                      'Seré tu acompañante esta vez para presentarte al equipo...'),
+                  TypewriterAnimatedText('Dejame presentartelos...')
+                ]),
             buildPage(
                 color: Colors.green,
                 backgroundImage: 'assets/backgroundRojo.png',
                 urlImage: 'assets/candelaPNG.png',
-                title: 'Bienvenido',
+                title: 'Team Valor',
                 subtitle:
-                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo'),
+                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo',
+                frases: [
+                  TypewriterAnimatedText('Soy la maestra Candela...'),
+                  TypewriterAnimatedText(
+                      'Soy la fundadora del Team Valor En Pokemon...'),
+                  TypewriterAnimatedText(
+                      'Nuestro principal fuerte son los pokemon de fuego...'),
+                  TypewriterAnimatedText(
+                      'Y cualquier tipo de poke con esas caracteristicas..'),
+                  TypewriterAnimatedText(
+                      'Ojala puedas unirte conmigo para atraparlos todos!'),
+                ]),
             buildPage(
                 color: Colors.green.shade100,
                 backgroundImage: 'assets/backgroundAmarillo.png',
                 urlImage: 'assets/sparkPNG.png',
-                title: 'Bienvenido',
+                title: 'Team Instinct',
                 subtitle:
-                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo'),
+                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo',
+                frases: [
+                  TypewriterAnimatedText('Mi nombre es Spark...'),
+                  TypewriterAnimatedText(
+                      'Soy el dueño del team Instinto o Amarillo en este caso...'),
+                  TypewriterAnimatedText(
+                      'Nuestro fuerte son los pokemon te tipo electricos...'),
+                  TypewriterAnimatedText(
+                      'Aunque obviamente tenemos experiencia con todo tipo...'),
+                  TypewriterAnimatedText(
+                      'Ojala podamos encontrarnos pronto para enseñarte mis pokes!')
+                ]),
             buildPage(
                 color: Colors.green.shade100,
                 backgroundImage: 'assets/backgroundAzul.png',
                 urlImage: 'assets/blanchePNG.png',
-                title: 'Bienvenido',
+                title: 'Team Mystic',
                 subtitle:
-                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo'),
+                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo',
+                frases: [
+                  TypewriterAnimatedText('Hola futuro Maestro Pokemon!'),
+                  TypewriterAnimatedText(
+                      'Mi nombre es Blanche y soy la fundadora del team Mystic'),
+                  TypewriterAnimatedText(
+                      'Aunque algunos nos dicen team azul, En este team...'),
+                  TypewriterAnimatedText(
+                      'Nos especializamos en todo tipo de pokemones...'),
+                  TypewriterAnimatedText(
+                      'Pero nuestros favoritos son los de agua!...'),
+                  TypewriterAnimatedText('Son Magnificos,¿No lo crees?')
+                ]),
             buildPage(
                 color: Colors.green.shade100,
                 backgroundImage: 'assets/backgroundWillow.png',
                 urlImage: 'assets/profesorWillow2.png',
-                title: 'Bienvenido',
+                title: 'POKEMON',
                 subtitle:
-                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo'),
+                    'Vas a empezar en el mundo pokemon, te presentaré a los equipos que existen en este mundo',
+                frases: [
+                  TypewriterAnimatedText(
+                      'Y estos son los mejores maestros pokemon...'),
+                  TypewriterAnimatedText(
+                      'Pero que opinas, ¿Te ha agradado algun team?...'),
+                  TypewriterAnimatedText(
+                      'Al final del día tendras que elegir alguno de ellos...'),
+                  TypewriterAnimatedText(
+                      'Pero por el momento vamos a capturar a todos los pokes'),
+                  TypewriterAnimatedText(
+                      'Que nos encontremos por el camino en tu nueva aventura...'),
+                  TypewriterAnimatedText('!Vamos alla!'),
+                ]),
           ],
         ),
       ),
@@ -147,6 +317,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
               },
             )
           : Container(
+              color: Theme.of(context).backgroundColor,
               padding: EdgeInsets.symmetric(horizontal: 10),
               height: 80,
               child: Row(
@@ -165,7 +336,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
                       effect: WormEffect(
                         spacing: 16,
                         dotColor: Colors.black26,
-                        activeDotColor: Colors.teal.shade700,
+                        activeDotColor: Theme.of(context).dialogBackgroundColor,
                       ),
                       onDotClicked: (index) {
                         controller.animateToPage(index,
--- lib/settings/styles_settings.dart
+++ lib/settings/styles_settings.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen_view/splash_screen_view.dart';
 
 ThemeData temaDia() {
   final ThemeData base = ThemeData.light();
-  return base.copyWith(backgroundColor: Colors.red);
+  return base.copyWith(
+      backgroundColor: Colors.red, dialogBackgroundColor: Colors.white);
 }
 
 ThemeData temaNoche() {
   final ThemeData base = ThemeData.dark();
-  return base.copyWith(backgroundColor: Color.fromARGB(255, 75, 17, 5));
+  return base.copyWith(
+    backgroundColor: Color.fromARGB(255, 75, 17, 5),
+    dialogBackgroundColor: Colors.white,
+  );
 }
 
 ThemeData temaCalido() {
@@ -20,6 +25,7 @@ ThemeData temaCalido() {
   return base.copyWith(
     splashColor: Colors.purple[300],
     backgroundColor: Colors.purple[300],
+    dialogBackgroundColor: Colors.purple[300],
     brightness: Brightness.light,
     secondaryHeaderColor: Colors.purple[300],
     scaffoldBackgroundColor: Colors.purple[100],
@@ -33,59 +39,113 @@ ThemeData temaCalido() {
   );
 }
 
-ThemeData temaPerso() {
-  final ThemeData base = ThemeData(
-      appBarTheme: AppBarTheme(
-        color: Colors.teal,
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          primary: Colors.teal,
-        ),
-      ),
-      scaffoldBackgroundColor: Colors.grey[200],
-      textTheme: TextTheme(
-        bodyText1: TextStyle(),
-        bodyText2: TextStyle(),
-      ).apply(
-        bodyColor: Colors.teal[800],
-      ));
-  return base.copyWith(
-      appBarTheme: AppBarTheme(
-        color: Colors.teal,
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          primary: Colors.teal,
-        ),
-      ),
-      scaffoldBackgroundColor: Colors.grey[200],
-      textTheme: TextTheme(
-        bodyText1: TextStyle(),
-        bodyText2: TextStyle(),
-      ).apply(
-        bodyColor: Colors.teal[800],
+ThemeData temaAmarillo() {
+  final ThemeData base = ThemeData.light();
+  // Gradient(colors: [Colors.amber, Colors.black26]);
+  // LinearGradient(colors: [Colors.red, Colors.blue]);
+  // return base.copyWith(
+  //     backgroundColor: Color.lerp(Colors.red, Colors.blue, 0.5));
+  return ThemeData(
+      primaryTextTheme: TextTheme(subtitle1: TextStyle(color: Colors.black)),
+      textTheme: TextTheme(labelLarge: TextStyle(color: Colors.black)),
+      splashColor: Colors.purple[300],
+      dialogBackgroundColor: Colors.purple,
+      backgroundColor: Color.fromARGB(255, 210, 229, 34),
+      brightness: Brightness.light,
+      secondaryHeaderColor: Colors.purple[300],
+      scaffoldBackgroundColor: Colors.purple[100],
+      colorScheme: ColorScheme.light(
+        onPrimary: Colors.purple,
+        onBackground: Colors.purple,
+        background: Colors.purple,
+        primary: Colors.purple,
+        secondary: Colors.purple,
       ));
 }
 
 ThemeData temaRojo() {
-  final ThemeData base = ThemeData.fallback();
-  return base.copyWith(backgroundColor: Color.fromARGB(255, 50, 26, 231));
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(backgroundColor: Color.fromARGB(255, 231, 26, 26));
 }
 
 ThemeData temaAzul() {
-  final ThemeData base = ThemeData.fallback();
-  return base.copyWith(backgroundColor: Color.fromARGB(255, 50, 26, 231));
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    backgroundColor: Color.fromARGB(255, 50, 26, 231),
+    dialogBackgroundColor: Colors.white,
+  );
 }
 
-ThemeData temaAmarillo() {
-  final ThemeData base = ThemeData.fallback();
-  //Gradient(colors: [Colors.amber, Colors.black26]);
-  //LinearGradient(colors: [Colors.red, Colors.blue]);
-  return base.copyWith(backgroundColor: Color.fromARGB(255, 50, 26, 231));
-}
-
-ThemeData temaPerso2() {
-  final ThemeData base = ThemeData();
-  return base.copyWith(backgroundColor: Color.fromARGB(255, 50, 26, 231));
-}
+// final ThemeData myTheme = ThemeData(
+//     primarySwatch: MaterialColor(4282339839,{50: Color( 0xffe5e8ff )
+// 		, 100: Color( 0xffccd1ff )
+// 		, 200: Color( 0xff99a3ff )
+// 		, 300: Color( 0xff6674ff )
+// 		, 400: Color( 0xff3346ff )
+// 		, 500: Color( 0xff0018ff )
+// 		, 600: Color( 0xff0013cc )
+// 		, 700: Color( 0xff000e99 )
+// 		, 800: Color( 0xff000a66 )
+// 		, 900: Color( 0xff000533 )
+// 		}),
+//     brightness: Brightness.light,
+//     primaryColor: Color( 0xff3f51ff ),
+//     primaryColorBrightness: Brightness.dark,
+//     primaryColorLight: Color( 0xffccd1ff ),
+//     primaryColorDark: Color( 0xff000e99 ),
+//     accentColor: Color( 0xff0018ff ),
+//     accentColorBrightness: Brightness.dark,
+//     canvasColor: Color( 0xfffafafa ),
+//     scaffoldBackgroundColor: Color( 0xfffafafa ),
+//     bottomAppBarColor: Color( 0xffffffff ),
+//     cardColor: Color( 0xffffffff ),
+//     dividerColor: Color( 0x1f000000 ),
+//     highlightColor: Color( 0x66bcbcbc ),
+//     splashColor: Color( 0x66c8c8c8 ),
+//     selectedRowColor: Color( 0xfff5f5f5 ),
+//     unselectedWidgetColor: Color( 0x8a000000 ),
+//     disabledColor: Color( 0x61000000 ),
+//     buttonColor: Color( 0xffe0e0e0 ),
+//     toggleableActiveColor: Color( 0xff0013cc ),
+//     secondaryHeaderColor: Color( 0xffe5e8ff ),
+//     textSelectionColor: Color( 0xff99a3ff ),
+//     cursorColor: Color( 0xff4285f4 ),
+//     textSelectionHandleColor: Color( 0xff6674ff ),
+//     backgroundColor: Color( 0xff99a3ff ),
+//     dialogBackgroundColor: Color( 0xffffffff ),
+//     indicatorColor: Color( 0xff0018ff ),
+//     hintColor: Color( 0x8a000000 ),
+//     errorColor: Color( 0xffd32f2f ),
+//     buttonTheme: ButtonThemeData(
+//       textTheme: ButtonTextTheme.normal,
+//       minWidth: 88,
+//       height: 36,
+//       padding: EdgeInsets.only(top:0,bottom:0,left:16, right:16),
+//       shape:     RoundedRectangleBorder(
+//       side: BorderSide(color: Color( 0xff000000 ), width: 0, style: BorderStyle.none, ),
+//       borderRadius: BorderRadius.all(Radius.circular(2.0)),
+//     ),
+//       alignedDropdown: false ,
+//       buttonColor: Color( 0xffe0e0e0 ),
+//       disabledColor: Color( 0x61000000 ),
+//       highlightColor: Color( 0x29000000 ),
+//       splashColor: Color( 0x1f000000 ),
+//       focusColor: Color( 0x1f000000 ),
+//       hoverColor: Color( 0x0a000000 ),
+//       colorScheme: ColorScheme(
+//         primary: Color( 0xff3f51ff ),
+//         primaryVariant: Color( 0xff000e99 ),
+//         secondary: Color( 0xff0018ff ),
+//         secondaryVariant: Color( 0xff000e99 ),
+//         surface: Color( 0xffffffff ),
+//         background: Color( 0xff99a3ff ),
+//         error: Color( 0xffd32f2f ),
+//         onPrimary: Color( 0xffffffff ),
+//         onSecondary: Color( 0xffffffff ),
+//         onSurface: Color( 0xff000000 ),
+//         onBackground: Color( 0xffffffff ),
+//         onError: Color( 0xffffffff ),
+//         brightness: Brightness.light,
+//       ),
+//     ),
+//   );
